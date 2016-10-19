c639dd49579892394f7485b0407bb2fd874cb7f9
sharaquss <sharaquss@gmail.com>
2016-10-20 01:43:29 +0200

    INFLATING FRAGMENT FROM CODE BELOW TOOLBAR YAAAAY

--- app/src/main/java/com/android/szparag/colortv/activites/MovieListActivity.java
+++ app/src/main/java/com/android/szparag/colortv/activites/MovieListActivity.java
@@ -1,11 +1,8 @@
 package com.android.szparag.colortv.activites;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.android.szparag.colortv.R;
 
@@ -18,14 +15,11 @@ public class MovieListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.activity_movie_list_fragment_placeholder,
+                new MovieListActivityFragment()
+        ).commit();
+
     }
 
 }
